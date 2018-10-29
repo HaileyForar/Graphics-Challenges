5be9bf6870be34e21ee345cc668a0a3b670d9731
hailey.forar <hailey.forar@MU-C1-15.mullen.pvt.k12.co.us>
2018-10-29 08:55:11 -0600

    Finish the checkerboard

--- Graphics Challenges/src/GraphicsChallenge10.java
+++ Graphics Challenges/src/GraphicsChallenge10.java
@@ -12,9 +12,9 @@ public class GraphicsChallenge10 extends Canvas
     		{
 	        GraphicsChallenge10 canvas = new GraphicsChallenge10();
 	        JFrame frame = new JFrame();
-	        frame.setSize(600, 400);
+	        frame.setSize(406, 420);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	        frame.getContentPane().add(canvas).setBackground(Color.black);
+	        frame.getContentPane().add(canvas).setBackground(Color.cyan);
 	        frame.setLocationRelativeTo(null); //Sets JPanel to center of screen 
 	        frame.setResizable(false);
 	        frame.setVisible(true);
@@ -23,8 +23,36 @@ public class GraphicsChallenge10 extends Canvas
     	public void paint(Graphics graphics) 
     		{
     				
-    			graphics.setColor(Color.cyan);
-    			graphics.fillRect(0, 0, 50, 400);
+    			for(int i = 0; i < 600; i = i + 50)
+    				{
+    					graphics.setColor(Color.darkGray);
+    					graphics.fillRect(i, i, 50, 50);
+    					
+    				}
+    			
+    			graphics.setColor(Color.darkGray);
+		        graphics.fillRect(100, 0, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(200, 0, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(300, 0, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(400, 0, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(0, 100, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(0, 200, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(0, 300, 50, 50);
+		        
+		        graphics.setColor(Color.darkGray);
+		        graphics.fillRect(0, 400, 50, 50);
 	       
 	      	}
 	}
