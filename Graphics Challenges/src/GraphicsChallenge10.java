72ea284ccd4c8aa42ae9f5bcd5d21fe718471ec0
Hailey <Hailey@10.22.22.103>
2018-10-31 08:56:03 -0600

    Finished the checkerboard

--- Graphics Challenges/src/GraphicsChallenge10.java
+++ Graphics Challenges/src/GraphicsChallenge10.java
@@ -12,7 +12,7 @@ public class GraphicsChallenge10 extends Canvas
     		{
 	        GraphicsChallenge10 canvas = new GraphicsChallenge10();
 	        JFrame frame = new JFrame();
-	        frame.setSize(406, 420);
+	        frame.setSize(400, 400);
 	        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	        frame.getContentPane().add(canvas).setBackground(Color.cyan);
 	        frame.setLocationRelativeTo(null); //Sets JPanel to center of screen 
@@ -22,40 +22,26 @@ public class GraphicsChallenge10 extends Canvas
  
     	public void paint(Graphics graphics) 
     		{
+    		int i = 0;
+    		int j = 0;
+
+    			for( i = 0; i < 400; i = i + 50)
+    			{
     				
-    			for(int i = 0; i < 600; i = i + 50)
+    				for( j = 0; j < 400; j = j + 100)
     				{
-    					graphics.setColor(Color.darkGray);
-    					graphics.fillRect(i, i, 50, 50);
+    					if((j == 0) && (i % 100 == 50))
+    					{
+    						j = j + 50;
+    					}
+    				graphics.setColor(Color.darkGray);
+					graphics.fillRect(j, i, 50, 50);
     					
     				}
-    			
-    			graphics.setColor(Color.darkGray);
-		        graphics.fillRect(100, 0, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(200, 0, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(300, 0, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(400, 0, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(0, 100, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(0, 200, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(0, 300, 50, 50);
-		        
-		        graphics.setColor(Color.darkGray);
-		        graphics.fillRect(0, 400, 50, 50);
-	       
-	      	}
-	}
+	      	   }
+    		}
+       }
+	
 	        
     
 	
